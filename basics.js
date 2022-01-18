3221bd163479a18ff40396f2e6b1f263f2bc0c0a
rashidulronye <rashidulislam.official1@gamil.com>
2022-01-18 14:18:59 +0600

    update

---
+++ basics.js
@@ -0,0 +1,34 @@
+/*Datatypes of Javascript
+1.Primitive Data type
+2. Non-primitive Data type
+
+Primitive Data-type
+1.Number*/
+const a = 20;
+const anotherNumber = 10.2;
+
+//2.string
+const name = "Rashidul Islam";
+const name1 = "Rashidul Islam";
+const name2 = `Rashidul Islam`;
+
+//3.Boolean
+const isWritting = false;
+const isEating = true;
+
+//4.Undefined
+// const myName;
+// console.log(myName);
+
+//null
+const something = null;
+console.log(something);
+
+//Non-primitive Data-type
+
+// 1.Array
+//concatenation
+const concat = `Rashidul` + `Rony`;
+console.log(concat);
+const type = typeof concat;
+console.log(type);
---
+++ operator.js
@@ -0,0 +1,32 @@
+/*
+Different kinds of operators in Javascript
+1. Arithmetic
+2.Comparison
+3.Logical/Relational
+4.Assignment
+5.Conditional/Ternary
+
+
+1.Arithmetic
+যোগ বিয়োগ গুণ ভাগ করার জন্য ব্যবহার করা হয়
+% বা মডুলাস
+++ (ইনক্রিমেন্ট)
+-- (ডিক্রিমেন্ট)
+
+
+2.Comparison
+===, !==, >, <, >=, <=
+
+
+3.Logical/Relational
+&&-and, ||-OR
+
+!-not
+
+4.Assignment
++=, -=, *=, /=, %=
+
+5.Conditional/Ternary
+(..?..:..)
+
+*/
